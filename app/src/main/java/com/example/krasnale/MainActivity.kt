45463ed24805635a09365cde77dbb4633f85718d
MaciekDanik <maciekdanik@gmail.com>
2024-04-30 23:33:23 +0200

    Reworked imageSelector and photoTaker

--- app/src/main/java/com/example/krasnale/MainActivity.kt
+++ app/src/main/java/com/example/krasnale/MainActivity.kt
@@ -1,18 +1,39 @@
 package com.example.krasnale
 
 import android.app.Activity
+import android.app.PendingIntent.getActivity
+import android.content.ContentValues
 import android.content.Intent
+import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.Manifest
+import android.annotation.SuppressLint
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.appcompat.app.AlertDialog
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Date
 
 class MainActivity : AppCompatActivity() {
     private  var selectedImageUri: Uri? = null
+
+    private val CAMERA_PERMISSION_CODE = 1000
+    private val READ_PERMISSION_CODE = 1001
+
+    private val IMAGE_CHOOSE = 1000
+    private val IMAGE_CAPTURE = 1001
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,43 +52,127 @@ class MainActivity : AppCompatActivity() {
 
 
         findViewById<ImageView>(R.id.imgView).setOnClickListener{
-            openImgChooser()
+            val permissionGranted = requestStoragePermission()
+
+            if (permissionGranted){
+                openImgChooser()
+            }
         }
 
         findViewById<Button>(R.id.btn_Upload).setOnClickListener{
             uploadImage()
         }
-    }
 
-    private fun openImgChooser() {
-        Intent(Intent.ACTION_PICK).also {
-            it.type = "image/*"
-            val mimeTypes = arrayOf("image/jpeg", "image/png")
-            it.putExtra(Intent.EXTRA_MIME_TYPES,mimeTypes)
-            startActivityForResult(it, REQUEST_CODE_IMAGE)
-        }
-    }
-
-    private fun uploadImage() {
-
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if (requestCode == Activity.RESULT_OK){
-            when(requestCode)
-            {
-                REQUEST_CODE_IMAGE -> {
-                    selectedImageUri = data?.data
-                    val ImV = findViewById<ImageView>(R.id.imgView)
-                    ImV.setImageURI(selectedImageUri)
-                }
+        findViewById<Button>(R.id.btn_TakePhoto).setOnClickListener {
+            val permissionGranted = requestCameraPermission()
+            if (permissionGranted){
+                takePhoto()
             }
         }
     }
 
-    companion object{
-        const val REQUEST_CODE_IMAGE = 101
+    private fun requestStoragePermission(): Boolean{
+        var permissionGranted = false
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            val storagePermissionNotGranted = ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_DENIED
+            if (storagePermissionNotGranted) {
+                val permission = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+                requestPermissions(permission, READ_PERMISSION_CODE)
+            } else {
+                permissionGranted = true
+            }
+
+        } else {
+            permissionGranted = true
+        }
+        return permissionGranted
+    }
+
+    private  fun requestCameraPermission(): Boolean{
+        var permissionGranted = false
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            val cameraPermissionsNotGranted = ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_DENIED
+            if (cameraPermissionsNotGranted){
+                val permission = arrayOf(Manifest.permission.CAMERA)
+                requestPermissions(permission, CAMERA_PERMISSION_CODE)
+            } else {
+                permissionGranted = true
+            }
+        } else {
+            permissionGranted = true
+        }
+        return permissionGranted
+    }
+
+    @SuppressLint("MissingSuperCall")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == CAMERA_PERMISSION_CODE){
+            if (grantResults.size == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                //openCameraInterface()
+                takePhoto()
+            } else {
+                //showAlert("Camera permission denied.")
+                Toast.makeText(this, "Camera permission denied.", Toast.LENGTH_SHORT).show()
+            }
+        } else if (requestCode == READ_PERMISSION_CODE){
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                val storagePermissionGranted = ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED
+                if (storagePermissionGranted){
+                    openImgChooser()
+                } else {
+                    //showAlert("Storage permission denied.")
+                    Toast.makeText(this, "Storage permission denied.", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                //showAlert("Storage permission not needed.")
+                Toast.makeText(this, "Storage permission not needed.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+    private fun openImgChooser() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_CHOOSE)
+    }
+
+    private fun uploadImage() {
+        //TO DO
+    }
+
+    private fun takePhoto(){
+        val values = ContentValues()
+        values.put(MediaStore.Images.Media.TITLE, R.string.take_picture)
+        selectedImageUri = contentResolver?.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+
+        //create camera intent
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, selectedImageUri)
+
+        //launch intent
+        startActivityForResult(intent, IMAGE_CAPTURE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        val ImV = findViewById<ImageView>(R.id.imgView)
+
+        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_CAPTURE){
+            ImV.setImageURI(selectedImageUri)
+        } else if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_CHOOSE){
+            ImV.setImageURI(data?.data)
+        }
     }
 }
